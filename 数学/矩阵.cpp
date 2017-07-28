40da4aeb0adf2a76c62096518623604bf8cb482e
XueHao <xuehaochina@gmail.com>
2017-07-28 11:24:28 +0800

    模板优化

--- 数学/矩阵.cpp
+++ 数学/矩阵.cpp
@@ -3,33 +3,37 @@
 using namespace std;
 #define MOD 1000000007
 
+const int MAXN=100;//矩阵的最大边长，根据题意修改
+int N;//矩阵边长
+
 struct Matrix
 {
-    int a[2][2];//矩阵大小根据需求修改
+    int a[MAXN][MAXN];//矩阵大小根据需求修改
     Matrix()
     {
         memset(a,0,sizeof(a));
     }
     void init()
     {
-        for(int i=0;i<2;i++)
-            for(int j=0;j<2;j++)
+        for(int i=0;i<N;i++)
+            for(int j=0;j<N;j++)
                 a[i][j]=(i==j);
     }
     Matrix operator + (const Matrix &B)const
     {
         Matrix C;
-        for(int i=0;i<2;i++)
-            for(int j=0;j<2;j++)
+        for(int i=0;i<N;i++)
+            for(int j=0;j<N;j++)
                 C.a[i][j]=(a[i][j]+B.a[i][j]+MOD)%MOD;
         return C;
     }
     Matrix operator * (const Matrix &B)const
     {
         Matrix C;
-        for(int i=0;i<2;i++)
-            for(int k=0;k<2;k++)
-                for(int j=0;j<2;j++)
+        for(int i=0;i<N;i++)
+            for(int k=0;k<N;k++)
+//                if(a[i][k])//当矩阵中0比较多时使用可以减少时间
+                for(int j=0;j<N;j++)
                     C.a[i][j]=(C.a[i][j]+1LL*a[i][k]*B.a[k][j]+MOD)%MOD;
         return C;
     }
