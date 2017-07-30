84bc32a1600ed96ca16eaf5839c8e1af340fde0c
XueHao <xuehaochina@gmail.com>
2017-07-30 09:47:57 +0800

    命名，注释更新
    防止命名冲突，修正注释

--- 其它/哈希.cpp
+++ 其它/哈希.cpp
@@ -1,19 +1,21 @@
+#include <cstring>
+
 struct HashMap
 {
-    const static int MOD=1196473;//比总状态数大一些即可
-    const static int MAXN=2000000;//大概状态数的两倍
-    int head[MOD];//链表头指针
-    int next[MAXN];//指向链表下一个节点
+    const static int mod=1196473;//比总状态数小一些即可
+    const static int maxn=2000000;//大概状态数大一些
+    int head[mod];//链表头指针
+    int next[maxn];//指向链表下一个节点
     int cnt;//当前节点数
-    int key[MAXN],val[MAXN];//键，值
+    int key[maxn],val[maxn];//键，值
     void clear()
     {
         cnt=0;
-        memset(head,0,sizeof head);
+        memset(head,-1,sizeof head);
     }
     inline void insert(int _key,int _val)
     {
-        int p=_key%MOD;//取模后对应的链
+        int p=_key%mod;//取模后对应的链
         ++cnt;
         key[cnt]=_key;
         val[cnt]=_val;
@@ -22,8 +24,8 @@ struct HashMap
     }
     int find(int _key)
     {
-        int p=_key%MOD;
-        for(int i=head[p];i;i=next[i])//沿着链找到目标值
+        int p=_key%mod;
+        for(int i=head[p];~i;i=next[i])//沿着链找到目标值
             if(val[i]==_key)
                 return val[i];
         return -1;//没找到
