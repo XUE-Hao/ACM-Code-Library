fe8a9ccd11f80ec3de9a9a8932b43c9a3857aa1b
XueHao <xuehaochina@gmail.com>
2017-10-26 11:01:19 +0800

    删除过于简单的内容

--- 数学/快速幂运算.cpp
+++
@@ -1,9 +0,0 @@
-long long mod_pow(long long x,long long n,long long mod)//计算x^n对mod去余
-{
-    if(n==0)
-        return 1;
-    long long res=mod_pow(x*x%mod,n/2,mod);
-    if(n&1)
-        res=res*x%mod;
-    return res;
-}
