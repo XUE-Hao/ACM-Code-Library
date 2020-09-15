7d2b53efb765b26c9b1afa63f09675b0ab7668ba
XueHao <xuehao@mails.ccnu.edu.cn>
2020-09-15 09:21:47 +0800

    创建头文件及宏模板

---
+++ 其它/头文件及宏.cpp
@@ -0,0 +1,16 @@
+#include <cstdio>
+#include <iostream>
+#include <cstdlib>
+#include <algorithm>
+#include <string>
+#include <cstring>
+#include <map>
+#include <queue>
+#include <stack>
+#include <list>
+using namespace std;
+#define LL long long
+#define INF 0x3f3f3f3f
+#define fi first
+#define se second
+#define mem(a,b) memset((a),(b),sizeof(a))
